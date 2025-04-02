3751de9bd1ca619e6b1937a13f396b814c2b966c
arshia <arshiabohloli@gmail.com>
2025-04-02 23:07:51 +0330

    validation for csrf added

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,6 +2,13 @@ package main
 
 import "net/http"
 
+const (
+	csrfTokenLength   = 32 // 32 bytes = 256 bits
+	csrfCookieName    = "csrf_token"
+	csrfHeaderName    = "X-CSRF-Token"
+	csrfFormFieldName = "csrf_token"
+)
+
 func csrfMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if isSafeMethod(r.Method) {
@@ -9,7 +16,25 @@ func csrfMiddleware(next http.Handler) http.Handler {
 			next.ServeHTTP(w, r)
 			return
 		}
+
+		if !validateCSRFToken(r) {
+			http.Error(w, "CSRF token validation failed", http.StatusForbidden)
+			return
+		}
 	})
+}
+
+func validateCSRFToken(r *http.Request) bool {
+	cookieToken, err := r.Cookie(csrfCookieName)
+	if err != nil {
+		return false
+	}
+
+	requestToken := r.Header.Get(csrfHeaderName)
+	if requestToken == "" {
+		requestToken = r.FormValue(csrfFormFieldName)
+	}
+	return cookieToken.Value == requestToken
 }
 
 func setCSRFToken(w http.ResponseWriter, r *http.Request) {
