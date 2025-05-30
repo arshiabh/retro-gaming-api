59ce6f2e5018119a449f6c00614bd6be76436f13
arshia <arshiabohloli@gmail.com>
2025-05-30 13:40:52 +0330

    fixed window rate limiting

--- internal/ratelimiter/fixed-window.go
+++ internal/ratelimiter/fixed-window.go
@@ -1,17 +1,45 @@
 package ratelimiter
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 type FixedWindowLimiter struct {
-	RequestPerTime int
-	TimeFrame      time.Duration
+	clients map[string]int
+	sync.RWMutex
+	window time.Duration
+	limit  int
 }
 
-func NewFixedWindowLimiter(request int, timeFrame time.Duration) *FixedWindowLimiter {
+func NewFixedWindowLimiter(limit int, window time.Duration) *FixedWindowLimiter {
 	return &FixedWindowLimiter{
-		RequestPerTime: request,
-		TimeFrame:      timeFrame,
+		window:  window,
+		limit:   limit,
+		clients: make(map[string]int),
 	}
 }
 
-func (r *FixedWindowLimiter) Allow(ip string) (bool, time.Duration)
+func (rl *FixedWindowLimiter) Allow(ip string) (bool, time.Duration) {
+	rl.RLock()
+	count, exist := rl.clients[ip]
+	if !exist || count < rl.limit {
+		rl.Lock()
+		if !exist {
+			go rl.resetCount(ip)
+		}
+
+		rl.clients[ip]++
+		rl.Unlock()
+		return true, 0
+	}
+
+	return false, rl.window
+}
+
+func (rl *FixedWindowLimiter) resetCount(ip string) {
+	time.Sleep(rl.window)
+	rl.Lock()
+	delete(rl.clients, ip)
+	rl.Unlock()
+}
